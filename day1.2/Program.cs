9c8680afa295b738ecfb621521d12154ca6eb0fe
Brian Butler <butler.brian.j@live.com>
2018-12-02 19:03:22 -0500

    Clone day1.2 from 1.1

---
+++ day1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace day1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+
+            int total = 0;
+            foreach (string line in lines)
+            {
+                int value = Int32.Parse(line);
+                total += value;
+                Console.WriteLine("Adding {0} to get the total {1}.", value, total);
+            }
+        }
+    }
+}
