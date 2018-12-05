61cd771eb894b85a85cbb63d3fdf4831a2b816f8
Brian Butler <butler.brian.j@live.com>
2018-12-04 21:50:16 -0500

    Squashed commit of the following:
    commit 07df6fd9b040bc8a5a301863e1e46b71be69f3e9
    Author: Brian Butler <butler.brian.j@live.com>
    Date:   Tue Dec 4 21:49:54 2018 -0500
    
        solve day 3.2
    
    commit bd7a55b6dd6eae485018b49d99f8cc32157accc4
    Author: Brian Butler <butler.brian.j@live.com>
    Date:   Mon Dec 3 21:56:38 2018 -0500
    
        Clone day3.1 to day3.2

---
+++ day3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace day3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+
+            List<Claim> claims = new List<Claim>();
+            foreach (string line in lines)
+                claims.Add(new Claim(line));
+
+            for (int i=0;i<claims.Count;i++) {
+                int nullCount=0;
+                for (int j=0;j<claims.Count;j++) {
+                    if (i==j) continue;
+                    if (claims[i].OverlapsWith(claims[j]) == null) nullCount++;
+                }
+                if (nullCount == claims.Count - 1) Console.WriteLine("Claim {0} has no overlaps.", claims[i].Id);
+            }
+        }
+    }
+}
